c91ad8adfdfab0709b0be455c6f3e229815b5f4d
hashir4560 <haashirraja@gmail.com>
2023-10-30 13:49:02 +0500

    Added Touchable Opacity and made changes

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
-import { useState } from "react";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "hashir", key: 1 },
-    { name: "Rahim", key: 2 },
-    { name: "Hakim", key: 3 },
-    { name: "Bilal", key: 4 },
-    { name: "peach", key: 5 },
-    { name: "apple", key: 6 },
-    { name: "bannaa", key: 7 },
-    { name: "mango", key: 8 },
-    { name: "guava", key: 9 },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
   ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
         numColumns={2}
         keyExtractor={(item) => item.id}
         data={people}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
-      {/* <ScrollView>
-        {people.map((item) => (
-          <View key={item.key}>
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
             <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -37,20 +44,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "ffff",
     paddingTop: 40,
     paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
-    // color: "blue",
+    backgroundColor: "#fff",
   },
   item: {
+    flex: 1,
+    marginHorizontal: 10,
     marginTop: 24,
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
-    fontWeight: "bold",
-    marginHorizontal: 10,
-    marginTop: 24,
   },
 });
+
+// {/* <ScrollView>
+//   {people.map((item) => (
+//     <View key={item.key}>
+//       <Text style={styles.item}>{item.name}</Text>
+//     </View>
+//   ))}
+// </ScrollView> */}
